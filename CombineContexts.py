e7be76c3281e1168b9688f4dbc35862e8e3cb10a
Valentin Wesp <VWesp>
2024-11-14 13:19:32 +0100

    update

--- CombineContexts.py
+++ CombineContexts.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     for step in range(1, 10, 1):
         step_output = os.path.join(output, str(step))
         step_data = os.path.join(data, str(step))
-        paths = [os.path.join(step_data, file) for file in os.listdir(step_data)]
+        paths = [os.path.join(step_data, file)
+                 for file in os.listdir(step_data)]
 
         dataframes = [pd.read_csv(path, sep="\t", header=0, index_col=0)
                       for path in paths]
@@ -29,15 +30,3 @@ if __name__ == "__main__":
         comb_sum_df = comb_sum_df.reindex(index=rows)
         res_output = os.path.join(type_output, f"{type}_context_sum.csv")
         comb_sum_df.to_csv(res_output, sep="\t")
-
-        '''freq_dfs = []
-        for df in dataframes:
-            freq_df = df.div(df.sum(axis=1), axis=0)
-            freq_dfs.append(freq_df)
-
-        combined_freq_df = pd.concat(freq_dfs)
-        comb_freq_med_df = combined_freq_df.groupby(combined_freq_df.index).agg("median")
-        comb_freq_med_df = comb_freq_med_df.reindex(index=rows)
-        comb_freq_med_df = comb_freq_med_df.div(comb_freq_med_df.sum(axis=1), axis=0)
-        res_output = os.path.join(type_output, f"{type}_context_freq_med.csv")
-        comb_freq_med_df.to_csv(res_output, sep="\t")'''
--- PlotContexts.py
+++ PlotContexts.py
@@ -12,21 +12,22 @@ import sklearn.decomposition as dec
 if __name__=="__main__":
     path,name,type = sys.argv[1:4]
 
-    codons = [f"{a}{b}{c}" for a in "ACGT" for b in "ACGT" for c in "ACGT"]
-    codons = [codon if i%4==0 else None for i,codon in enumerate(codons)]
+    all_codons = [f"{a}{b}{c}" for a in "ACGT" for b in "ACGT" for c in "ACGT"]
+    codons = [codon if i%4==0 else None for i,codon in enumerate(all_codons)] + [all_codons[-1]]
     amino_acids = ["M", "W", "C", "D", "E", "F", "H", "K", "N", "Q", "Y", "I",
                    "A", "G", "P", "T", "V", "L", "R", "S"]
 
     type_output = os.path.join(path, type)
     os.makedirs(type_output, exist_ok=True)
-    
+
     for calc in ["sum", "median"]:
         vmin = 0
         vmax = 0
         context_frames = []
         for step in range(1, 10, 1):
             type_path = os.path.join(os.path.join(path, str(step)), type)
-            context_df = pd.read_csv(os.path.join(type_path, f"{type}_context_{calc}.csv"),
+            context_df = pd.read_csv(os.path.join(type_path,
+                                                  f"{type}_context_{calc}.csv"),
                                      sep="\t", header=0, index_col=0)
             context_df = context_df.drop(["Start", "End"])
             context_frames.append(context_df)
@@ -41,33 +42,34 @@ if __name__=="__main__":
                         vmin=vmin, vmax=vmax, ax=axes[j,i])
 
             if(type == "codon"):
-                axes[j,i].set_xticks(np.arange(len(codons))+0.5, codons, fontsize=8)
-                axes[j,i].set_yticks(np.arange(len(codons))+0.5, codons, fontsize=8)
+                axes[j,i].set_xticks(np.arange(len(codons)), codons, fontsize=8)
+                axes[j,i].set_yticks(np.arange(len(codons)), codons, fontsize=8)
             elif(type == "amino"):
-                axes[j,i].set_xticks(np.arange(len(amino_acids))+0.5, amino_acids, fontsize=8)
-                axes[j,i].set_yticks(np.arange(len(amino_acids))+0.5, amino_acids, fontsize=8)
+                axes[j,i].set_xticks(np.arange(len(amino_acids))+0.5, amino_acids,
+                                     fontsize=8)
+                axes[j,i].set_yticks(np.arange(len(amino_acids))+0.5, amino_acids,
+                                     fontsize=8)
 
             axes[j,i].set_title(f"Neighbor distance: {step+1}")
             i = i+1 if i < 2 else 0
             j = j+1 if i == 0 else j
 
-        plt.subplots_adjust(hspace=0.4, wspace=0.4)
-        fig.suptitle(f"{name} - Neighboring {type} {calc} counts", fontsize=14, y=0.93)
+
+        fig.suptitle(f"{name} - Neighboring {type} {calc} counts", fontsize=14,
+                     y=0.93)
 
         for ext in ["svg", "pdf"]:
-            plt.savefig(f"{type_output}/{type}_context_{calc}_counts.{ext}", bbox_inches="tight")
+            plt.savefig(f"{type_output}/{type}_context_{calc}_counts.{ext}",
+                        bbox_inches="tight")
 
         plt.close()
 
-    #########################################################
+        #########################################################
 
-    for calc in ["sum", "median"]:
         vmin = 0
         vmax = 0
         z_frames = []
         for df in context_frames:
-            row_means = df.mean(axis=1)
-            row_stds = df.std(axis=1)
             z_score_df = sci.zscore(df)
             z_frames.append(z_score_df)
             vmin = min(vmin, np.min(z_score_df))
@@ -82,31 +84,34 @@ if __name__=="__main__":
             axes[j,i].set_title(f"Neighbor distance: {step+1}")
 
             if(type == "codon"):
-                axes[j,i].set_xticks(np.arange(len(codons))+0.5, codons, fontsize=8)
-                axes[j,i].set_yticks(np.arange(len(codons))+0.5, codons, fontsize=8)
+                axes[j,i].set_xticks(np.arange(len(codons)), codons, fontsize=8)
+                axes[j,i].set_yticks(np.arange(len(codons)), codons, fontsize=8)
             elif(type == "amino"):
-                axes[j,i].set_xticks(np.arange(len(amino_acids))+0.5, amino_acids, fontsize=8)
-                axes[j,i].set_yticks(np.arange(len(amino_acids))+0.5, amino_acids, fontsize=8)
+                axes[j,i].set_xticks(np.arange(len(amino_acids))+0.5, amino_acids,
+                                     fontsize=8)
+                axes[j,i].set_yticks(np.arange(len(amino_acids))+0.5, amino_acids,
+                                     fontsize=8)
 
             csv_output = os.path.join(os.path.join(path, str(step+1)), type)
-            df.to_csv(os.path.join(csv_output, f"{type}_context_{calc}_z_scores.csv"),
+            df.to_csv(os.path.join(csv_output,
+                                   f"{type}_context_{calc}_z_scores.csv"),
                       sep="\t")
 
             i = i+1 if i < 2 else 0
             j = j+1 if i == 0 else j
 
-        plt.subplots_adjust(hspace=0.4, wspace=0.4)
-        fig.suptitle(f"{name} - Neighboring {type} {calc} Z-scores", fontsize=14, y=0.93)
+        fig.suptitle(f"{name} - Neighboring {type} {calc} Z-scores",
+                     fontsize=14, y=0.93)
 
         for ext in ["svg", "pdf"]:
-            plt.savefig(f"{type_output}/{type}_context_{calc}_z_scores.{ext}", bbox_inches="tight")
+            plt.savefig(f"{type_output}/{type}_context_{calc}_z_scores.{ext}",
+                        bbox_inches="tight")
 
         plt.close()
 
-    #########################################################
+        #########################################################
 
-    pca = dec.PCA(n_components=2)
-    for calc in ["sum", "median"]:
+        pca = dec.PCA(n_components=2)
         fig,axes = plt.subplots(3, 3, figsize=(15, 15))
         i = 0
         j = 0
@@ -121,22 +126,62 @@ if __name__=="__main__":
             adjustText.adjust_text(texts, arrowprops=dict(arrowstyle="-",
                                    color="gray", lw=0.5), ax=axes[j,i])
 
-            axes[j,i].set_xticks([])
-            axes[j,i].set_yticks([])
+            if(j != 2):
+                axes[j,i].set_xticks([])
+
+            if(i != 0):
+                axes[j,i].set_yticks([])
+
+            plt.setp(axes[j,i].get_xticklabels(), rotation=30,
+                     horizontalalignment="right")
+
             axes[j,i].set_title(f"Neighbor distance: {step+1}")
 
-            pca_df = pd.DataFrame(pca_result, columns=["PC1","PC2"], index=df.index)
+            pca_df = pd.DataFrame(pca_result, columns=["PC1","PC2"],
+                                  index=df.index)
             csv_output = os.path.join(os.path.join(path, str(step+1)), type)
-            pca_df.to_csv(os.path.join(csv_output, f"{type}_context_{calc}_pcas.csv"),
+            pca_df.to_csv(os.path.join(csv_output,
+                          f"{type}_context_{calc}_pcas.csv"),
                           sep="\t")
 
             i = i+1 if i < 2 else 0
             j = j+1 if i == 0 else j
 
-        plt.subplots_adjust(hspace=0.4, wspace=0.4)
-        fig.suptitle(f"{name} - Neighboring {type} {calc} PCA", fontsize=14, y=0.93)
+        fig.suptitle(f"{name} - Neighboring {type} {calc} PCA", fontsize=14,
+                     y=0.93)
 
         for ext in ["svg", "pdf"]:
-            plt.savefig(f"{type_output}/{type}_context_{calc}_pcas.{ext}", bbox_inches="tight")
+            plt.savefig(f"{type_output}/{type}_context_{calc}_pcas.{ext}",
+                        bbox_inches="tight")
 
         plt.close()
+
+        #########################################################
+
+        if(type == "amino"):
+            fig,axes = plt.subplots(4, 5, figsize=(20, 15))
+            cm = plt.get_cmap("viridis")
+            colors = [cm(i/len(amino_acids)) for i in range(len(amino_acids))]
+            i = 0
+            j = 0
+            for aa in amino_acids:
+                data = context_frames[0].loc[aa]
+                axes[j,i].bar(np.arange(len(amino_acids)), data, color=colors)
+                axes[j,i].set_ylim(0, np.max(context_frames[0]*1.05))
+                axes[j,i].set_xticks(np.arange(len(amino_acids)), amino_acids)
+                axes[j,i].set_title(f"Amino acid: {aa}")
+                if(i != 0):
+                    axes[j,i].set_yticks([])
+
+                i = i+1 if i < 4 else 0
+                j = j+1 if i == 0 else j
+
+            plt.subplots_adjust(hspace=0.3, wspace=0.05)
+            fig.suptitle(f"{name} - Amino acids neighbor {calc} counts",
+                         fontsize=14, y=0.93)
+
+            for ext in ["svg", "pdf"]:
+                plt.savefig(f"{type_output}/{type}_context_{calc}_comp.{ext}",
+                            bbox_inches="tight")
+
+            plt.close()
