a539958a7be399d692da4bc61835193a6bef7416
Valentin Wesp <VWesp>
2025-03-21 17:28:14 +0100

    update

---
+++ ProteomeCodonDistribution.sh
@@ -0,0 +1,19 @@
+# Archaea tr
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Archaea/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Archaea/codon_dis/tr tr 60 &&
+# Archaea sp
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Archaea/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Archaea/codon_dis/sp/ sp 60 &&
+
+# Bacteria tr
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Bacteria/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Bacteria/codon_dis/tr/ tr 60 &&
+# Bacteria sp
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Bacteria/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Bacteria/codon_dis/sp/ sp 60 &&
+
+# Eukaryotes tr
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Eukaryotes/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Eukaryotes/codon_dis/tr/ tr 60 &&
+# Eukaryotes sp
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Eukaryotes/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Eukaryotes/codon_dis/sp/ sp 60 &&
+
+# Viruses tr
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Viruses/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Viruses/codon_dis/tr/ tr 60 &&
+# Viruses sp
+~/anaconda3/bin/python3 ProteomeCodonDistribution.py /media/valentin-wesp/Volume/uniprot_proteomes/Viruses/data/ /media/valentin-wesp/Volume/uniprot_proteomes/Viruses/codon_dis/sp/ sp 60
