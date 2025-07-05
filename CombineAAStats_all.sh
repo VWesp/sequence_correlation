72e6986d440fd58a69e0184134fb5eb69c3a28cf
VWesp <valentinwesp@yahoo.de>
2025-07-05 17:14:10 +0200

    update

--- CombineAAStats_all.sh
+++ CombineAAStats_all.sh
@@ -1,4 +1,5 @@
 for domain in "Archaea" "Bacteria" "Eukaryota" "Viruses"; do
+	echo $domain
 	/root/anaconda3/bin/python3 GetGeneticEncoding.py -d /home/vwesp/uniprot_knowledgebase/$domain/aa_distributions/ -o /home/vwesp/uniprot_knowledgebase/$domain/encoding_data.csv &&
 	/root/anaconda3/bin/python3 CombineAAStats.py -d /home/vwesp/uniprot_knowledgebase/$domain/aa_distributions/ -o /home/vwesp/uniprot_knowledgebase/$domain/ -e /home/vwesp/uniprot_knowledgebase/$domain/encoding_data.csv -c genetic_codes/ -m code_map.csv -r 10000 -ch 1000 -t 20
 	echo ""
