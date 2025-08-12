0d60411b22ddaf9d23dc9a982077afbd200ca75d
VWesp <valentinwesp@yahoo.de>
2025-08-12 16:14:44 +0200

    update

--- CombineAAStats.py
+++ CombineAAStats.py
@@ -10,6 +10,17 @@ import sklearn.metrics as skl
 import equation_functions as ef
 
 
+def fisher_Z(x):
+	### Fisher's Z-transformation
+	# Correlations
+	z = [0.5*np.log((1+r)/(1-r)) for r in x]
+	mean_z = np.mean(z)
+	std_z = 1 / np.sqrt(len(x)-3)
+	mean_r = (np.exp(2*mean_z)-1) / (np.exp(2*mean_z)+1)
+	std_r = (np.exp(2*std_z)-1) / (np.exp(2*std_z)+1)
+	return [mean_r, std_r]
+
+
 def combine_distribution_stats(data):
 	tax_id,dis_df,code_name,freq_funcs,resamples = data
 
@@ -23,6 +34,8 @@ def combine_distribution_stats(data):
 
 	dis_df.fillna(0.0, inplace=True)
 	dis_sr = pd.Series(name=tax_id)
+	dis_sr["Genetic_code"] = code_name		
+	dis_sr["#Proteins"] = len(dis_df)
 	dis_sr["Length_median"] = dis_df["Length"].median()
 	dis_sr["Length_mad"] = (dis_df["Length"] - dis_sr["Length_median"]).abs().median()
 	dis_sr["GC_median"] = dis_df["GC"].median()
@@ -39,18 +52,23 @@ def combine_distribution_stats(data):
 			dis_sr[f"{aa}_median"] = 0.0
 			dis_sr[f"{aa}_mad"] = 0.0
 		
-		############################ Percentage change between empirical and code data
+		fold_nom = dis_sr[f"{aa}_median"] if dis_sr[f"{aa}_median"] > 0 else 1e-10
+		############################ log-Fold change and percentage change between empirical and code data
 		dis_sr[f"{aa}_code"] = code_freq_func["amino"][one_letter_code[aa]]
-		dis_sr[f"{aa}_code_pct"] = (dis_sr[f"{aa}_median"] - dis_sr[f"{aa}_code"]) / ((dis_sr[f"{aa}_median"] + dis_sr[f"{aa}_code"]) / 2)
-		
-		############################ Percentage change between empirical and frequency data
+		dis_sr[f"{aa}_code_lfc"] = np.log(fold_nom / dis_sr[f"{aa}_code"])
+		dis_sr[f"{aa}_code_pct"] = (dis_sr[f"{aa}_median"] - dis_sr[f"{aa}_code"]) / dis_sr[f"{aa}_code"]
+		############################ log-Fold change and percentage change between empirical and frequency data
 		dis_sr[f"{aa}_gc"] = gc_freq_func["amino"][one_letter_code[aa]]
-		dis_sr[f"{aa}_gc_pct"] = (dis_sr[f"{aa}_median"] - dis_sr[f"{aa}_gc"]) / ((dis_sr[f"{aa}_median"] + dis_sr[f"{aa}_gc"]) / 2)
+		dis_sr[f"{aa}_gc_lfc"] = np.log(fold_nom / dis_sr[f"{aa}_gc"])
+		dis_sr[f"{aa}_gc_pct"] = (dis_sr[f"{aa}_median"] - dis_sr[f"{aa}_gc"]) / dis_sr[f"{aa}_gc"]
 	
 
 	aa_median_cols = [f"{aa}_median" for aa in amino_acids]
 	code_freq_cols = [f"{aa}_code" for aa in amino_acids]
 	gc_freq_cols = [f"{aa}_gc" for aa in amino_acids]
+	############################ log-RMSE
+	dis_sr["log_RMSE_code"] = np.log(skl.root_mean_squared_error(dis_sr[aa_median_cols], dis_sr[code_freq_cols]))
+	dis_sr["log_RMSE_gc"] = np.log(skl.root_mean_squared_error(dis_sr[aa_median_cols], dis_sr[gc_freq_cols]))
 	############################ Pearson code
 	dis_sr["Ps_code"] = sci.pearsonr(dis_sr[aa_median_cols], dis_sr[code_freq_cols]).statistic
 	dis_sr["Ps_code_p"] = sci.permutation_test((dis_sr[aa_median_cols],), lambda x: sci.pearsonr(x, dis_sr[code_freq_cols]).statistic, permutation_type="pairings", 
@@ -74,14 +92,8 @@ def combine_distribution_stats(data):
 	############################ Kendall tau frequency
 	dis_sr["Kt_gc"] = sci.kendalltau(dis_sr[aa_median_cols], dis_sr[gc_freq_cols], nan_policy="raise").statistic
 	dis_sr["Kt_gc_p"]  = sci.permutation_test((dis_sr[aa_median_cols],), lambda x: sci.kendalltau(x, dis_sr[gc_freq_cols]).statistic, permutation_type="pairings",
-																								  n_resamples=resamples).pvalue												
-	
-	############################ Code frequency RMSE
-	dis_sr["RMSE_code"] = skl.root_mean_squared_error(dis_sr[aa_median_cols], dis_sr[code_freq_cols])
-	############################ GC frequency RMSE
-	dis_sr["RMSE_gc"] = skl.root_mean_squared_error(dis_sr[aa_median_cols], dis_sr[gc_freq_cols])
-	
-	dis_sr["Genetic_code"] = code_name																							
+																								  n_resamples=resamples).pvalue
+																						
 	return dis_sr.to_frame().T
 
 
@@ -91,7 +103,7 @@ if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser(description="Compute amino acid distributions in proteomes")
 	parser.add_argument("-d", "--data", help="Specify the path to the folder with the distribution files", required=True)
-	parser.add_argument("-o", "--output", help="Specify the output file", required=True)
+	parser.add_argument("-o", "--output", help="Specify the output folder", required=True)
 	parser.add_argument("-e", "--encoding", help="Set the path to the encoding file", required=True)
 	parser.add_argument("-c", "--codes", help="Specify the path to the folder with the genetic code files", required=True)
 	parser.add_argument("-m", "--mapping", help="Set the path to the mappings of the genetic codes", required=True)
@@ -109,6 +121,8 @@ if __name__ == "__main__":
 	chunk_size = args.chunks
 	threads = args.threads
 	
+	os.makedirs(output, exist_ok=True)
+	
 	encoding_df = pd.read_csv(encoding, sep="\t", header=0, index_col=0).fillna("1")
 	code_map_df = pd.read_csv(code_map, sep="\t", header=0, index_col=0)
 	dis_files = os.listdir(data_path)
@@ -116,6 +130,7 @@ if __name__ == "__main__":
 	if(chunk_size <= 0):
 		chunk_size = len(dis_files)
 	
+	frames = []
 	with mp.Pool(processes=threads) as pool:
 		for chunk in range(0, len(dis_files), chunk_size):
 			dis_data = []
@@ -136,9 +151,39 @@ if __name__ == "__main__":
 			result = list(tqdm.tqdm(pool.imap(combine_distribution_stats, dis_data), total=len(dis_data), desc=f"Calculating amino acid statistics for chunk " 
 																											   f"[{chunk}-{max_chunk}/{len(dis_files)}]"))
 			for res in result:
-				comb_dis_df = pd.concat([comb_dis_df, res])
-
+				frames.append(res)
+				
+	
+	comb_dis_df = pd.concat(frames)
 	comb_dis_df.astype(str).fillna("0.0", inplace=True)
-	comb_dis_df.index.name = "Prot_Tax_ID"
-	comb_dis_df.to_csv(output, sep="\t")
+	comb_dis_df.index.name = "TaxID"
+	comb_dis_df.to_csv(os.path.join(output, "combined_distributions.csv"), sep="\t")
+	
+	### Summarize data
+	corr_start_idx = [i for i,col in enumerate(df.columns) if col.startswith("Ps_")][0]
+	summary_df = pd.DataFrame(index=["Sum", "Median", "MAD", "Min", "Max", "25%", "75%"])
+	# Summarize non correlation data
+	for col in comb_dis_df.columns[1:corr_start_idx]:
+		if(not col.endswith("_mad"):
+			summary_df.loc["Sum", col] = comb_dis_df[col].sum()
+			summary_df.loc["Mean", col] = comb_dis_df[col].mean()
+			summary_df.loc["Std", col] = comb_dis_df[col].std()
+			summary_df.loc["Min", col] = comb_dis_df[col].min()
+			summary_df.loc["Max", col] = comb_dis_df[col].max()
+			summary_df.loc["25%", col] = comb_dis_df[col].quantile(0.25)
+			summary_df.loc["75%", col] = comb_dis_df[col].quantile(0.75)
+		
+	# Summarize correlation data
+	for col in comb_dis_df.columns[corr_start_idx:]:
+		summary_df.loc["Sum", col] = comb_dis_df[col].sum()
+		mean,std = fisher_Z(comb_dis_df[col])
+		summary_df.loc["Mean", col] = mean
+		summary_df.loc["Std", col] = std
+		summary_df.loc["Min", col] = comb_dis_df[col].min()
+		summary_df.loc["Max", col] = comb_dis_df[col].max()
+		summary_df.loc["25%", col] = comb_dis_df[col].quantile(0.25)
+		summary_df.loc["75%", col] = comb_dis_df[col].quantile(0.75)
+		
+	summary_df.to_csv(os.path.join(output, "distribution_description.csv"), sep="\t")
+	
 		
--- Pipeline.sh
+++ Pipeline.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-for domain in "Archaea" "Viruses" "Bacteria" "Eukaryota"; do
+for domain in "Archaea"; do
 	echo $domain
 	/home/we63kel/anaconda3/bin/python3 DownloadProteome.py -d $domain -o /home/we63kel/uniprot_knowledgebase -t 5 -w 15 &&
-	/home/we63kel/anaconda3/bin/python3 ProteomeAADistribution.py -d /home/we63kel/uniprot_knowledgebase/$domain/data -o /home/we63kel/uniprot_knowledgebase/$domain/aa_distributions -c 1000 -t 60 &&
+	/home/we63kel/anaconda3/bin/python3 ProteomeAADistribution.py -d /home/we63kel/uniprot_knowledgebase/$domain/data -o /home/we63kel/uniprot_knowledgebase/$domain/aa_distributions -c -1 -t 60 &&
 	/home/we63kel/anaconda3/bin/python3 GetGeneticEncoding.py -d /home/we63kel/uniprot_knowledgebase/$domain/aa_distributions/ -o /home/we63kel/uniprot_knowledgebase/$domain/encoding_data.csv &&
-	/home/we63kel/anaconda3/bin/python3 CombineAAStats.py -d /home/we63kel/uniprot_knowledgebase/$domain/aa_distributions/ -o /home/we63kel/uniprot_knowledgebase/$domain/combined_distributions.csv -e /home/we63kel/uniprot_knowledgebase/$domain/encoding_data.csv -c genetic_codes/ -m code_map.csv -r 10000 -ch 1000 -t 60
-	/home/we63kel/anaconda3/bin/python3 DataAggregation.py -d /home/we63kel/uniprot_knowledgebase/$domain/aa_distributions/ -o /home/we63kel/uniprot_knowledgebase/$domain/distribution_description.csv
+	/home/we63kel/anaconda3/bin/python3 CombineAAStats.py -d /home/we63kel/uniprot_knowledgebase/$domain/aa_distributions/ -o /home/we63kel/uniprot_knowledgebase/$domain/ -e /home/we63kel/uniprot_knowledgebase/$domain/encoding_data.csv -c genetic_codes/ -m code_map.csv -r 10000 -ch -1 -t 60
 	echo ""
 done
 
-echo "Plotting statistics..."
-/home/we63kel/anaconda3/bin/python3 PlotAAStats.py -i /home/we63kel/uniprot_knowledgebase/ -o /home/we63kel/uniprot_knowledgebase/stats_results -r 10000
+#echo "Plotting statistics..."
+#/home/we63kel/anaconda3/bin/python3 PlotAAStats.py -i /home/we63kel/uniprot_knowledgebase/ -o /home/we63kel/uniprot_knowledgebase/stats_results -r 10000
